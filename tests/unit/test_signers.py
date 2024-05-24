8fd96d9abd777a7dd0b4a47e1a831218fb047d1b
Nate Prewitt <nate.prewitt@gmail.com>
2024-05-23 21:01:45 -0600

    Add base async unit tests

--- tests/unit/test_signers.py
+++ tests/unit/test_signers.py
@@ -9,8 +9,15 @@ from datetime import UTC, datetime
 from io import BytesIO
 
 import pytest
-from aws_sdk_signers import URI, AWSCredentialIdentity, AWSRequest, Fields
-from aws_sdk_signers.signers import SigV4Signer, SigV4SigningProperties
+from aws_sdk_signers import (
+    URI,
+    AsyncSigV4Signer,
+    AWSCredentialIdentity,
+    AWSRequest,
+    Fields,
+    SigV4Signer,
+    SigV4SigningProperties,
+)
 
 SIGV4_RE = re.compile(
     r"AWS4-HMAC-SHA256 "
@@ -96,6 +103,57 @@ class TestSigV4Signer:
         )
         with pytest.raises(ValueError):
             self.SIGV4_SYNC_SIGNER.sign(
+                signing_properties=signing_properties,
+                request=aws_request,
+                identity=identity,
+            )
+
+
+class TestAsyncSigV4Signer:
+    SIGV4_ASYNC_SIGNER = AsyncSigV4Signer()
+
+    async def test_sign(
+        self,
+        aws_identity: AWSCredentialIdentity,
+        aws_request: AWSRequest,
+        signing_properties: SigV4SigningProperties,
+    ) -> None:
+        signed_request = await self.SIGV4_ASYNC_SIGNER.sign(
+            signing_properties=signing_properties,
+            request=aws_request,
+            identity=aws_identity,
+        )
+        assert isinstance(signed_request, AWSRequest)
+        assert signed_request is not aws_request
+        assert "authorization" in signed_request.fields
+        authorization_field = signed_request.fields["authorization"]
+        assert SIGV4_RE.match(authorization_field.as_string())
+
+    @typing.no_type_check
+    async def test_sign_with_invalid_identity(
+        self, aws_request: AWSRequest, signing_properties: SigV4SigningProperties
+    ) -> None:
+        """Ignore typing as we're testing an invalid input state."""
+        identity = object()
+        assert not isinstance(identity, AWSCredentialIdentity)
+        with pytest.raises(ValueError):
+            await self.SIGV4_ASYNC_SIGNER.sign(
+                signing_properties=signing_properties,
+                request=aws_request,
+                identity=identity,
+            )
+
+    async def test_sign_with_expired_identity(
+        self, aws_request: AWSRequest, signing_properties: SigV4SigningProperties
+    ) -> None:
+        identity = AWSCredentialIdentity(
+            access_key_id="AKID123456",
+            secret_access_key="EXAMPLE1234SECRET",
+            session_token="X123456SESSION",
+            expiration=datetime(1970, 1, 1, tzinfo=UTC),
+        )
+        with pytest.raises(ValueError):
+            await self.SIGV4_ASYNC_SIGNER.sign(
                 signing_properties=signing_properties,
                 request=aws_request,
                 identity=identity,
