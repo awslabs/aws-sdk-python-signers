fff6297577159caf6934a26c5efa3e5beb91231c
Ryan DiRisio <rdirisio@treeline.bio>
2024-08-16 15:00:20 -0400

    remove unnecessary timezone import

--- tests/unit/test_signers.py
+++ tests/unit/test_signers.py
@@ -6,7 +6,7 @@ SPDX-License-Identifier: Apache-2.0
 import re
 import sys
 import typing
-from datetime import datetime, timezone
+from datetime import datetime
 from io import BytesIO
 
 import pytest
